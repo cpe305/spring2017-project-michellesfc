cfc7c24b89d8dbe4e729173fa6d23106669277b5
michelle <michellesfc@gmail.com>
2017-04-22 22:11:36 -0700

    Adding toolbar menu

--- app/src/main/java/com/example/michellegu/avoid/MainActivity.java
+++ app/src/main/java/com/example/michellegu/avoid/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.michellegu.avoid;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.Spanned;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -20,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
     private TextView mPlaceDetailsText;
-
+    Intent intent = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +35,23 @@ public class MainActivity extends AppCompatActivity {
 
         mPlaceDetailsText = (TextView) findViewById(R.id.placeTextView);
 
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+
+
         try {
-            Intent intent =
-                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
-                            .build(this);
-            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+            intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                    .build(this);
         } catch (GooglePlayServicesRepairableException e) {
             // TODO: Handle the error.
         } catch (GooglePlayServicesNotAvailableException e) {
             // TODO: Handle the error.
         }
+
+
     }
 
     @Override
@@ -48,11 +62,9 @@ public class MainActivity extends AppCompatActivity {
                 mPlaceDetailsText.setText(formatPlaceDetails(getResources(), place.getName(),
                         place.getId(), place.getAddress(), place.getPhoneNumber(),
                         place.getWebsiteUri()));
-                //Log.i(TAG, "Place: " + place.getName());
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
                 Status status = PlaceAutocomplete.getStatus(this, data);
                 // TODO: Handle the error.
-                //Log.i(TAG, status.getStatusMessage());
 
             } else if (resultCode == RESULT_CANCELED) {
                 // The user canceled the operation.
@@ -66,6 +78,37 @@ public class MainActivity extends AppCompatActivity {
         return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
                 websiteUri));
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                System.out.println("settings clicked");
+                Intent nextActivity = new Intent(this, Main2Activity.class);
+                startActivity(nextActivity);
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+            case R.id.app_bar_search:
+                startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                System.out.println("search clicked");
+                return true;
+            default:
+                System.out.println(" clicked");
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 }
