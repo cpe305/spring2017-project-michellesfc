9d3b407e5f72d6406fad453acef2f7d74aeed7a3
Gudrun Socher <gudrun.socher@hm.edu>
2017-05-09 02:06:50 -0700

    UserTests added

---
+++ app/src/test/java/com/example/michellegu/avoid/UserTests.java
@@ -0,0 +1,34 @@
+package com.example.michellegu.avoid.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.Test;
+import static org.junit.Assert.*; 
+
+
+public class UserTests {
+
+    User user = new User();
+
+    @Test
+    public void testUserName() {
+        user.setName("Jane");
+        assertEquals("Jane", user.getName());
+    }
+
+    @Test
+    public void testUserNameEmpty() {
+        user.setName("");
+        assertEquals("", user.getName());
+    }
+
+    @Test
+    public void testUserGetFavoritePlaces() {
+        List<PlaceData> favoritePlaces = new ArrayList<PlaceData>;
+        PlaceData placeData = new PlaceData("203", "Lecture Room 203", "Cal Poly, SLO", 10);
+        favoritePlaces.add(placeData);
+        user.setFavoritePlaces(favoritePlaces);
+        assertEquals(user.getFavoritePlaces().get(0).getPlaceID(), "203");
+    }
+}
